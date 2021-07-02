e36af7983c40ab653368e542a3081aa078d1ff22
Vitaly <novaua@ukr.net>
2021-07-02 14:40:54 +0300

    License added

--- ExprCalculator.h
+++ ExprCalculator.h
@@ -1,4 +1,24 @@
+/*
+ ******************************************************************
+ *           C++ Basic Mathematical Calculator                    *
+ *                                                                *
+ * Author: Vitaly German (2021)                                   *
+ * URL: https://github.com/novaua/							      *
+ *                                                                *
+ * Copyright notice:                                              *
+ * Free use of the  C++ Basic Mathematical Calculator is          *
+ * permitted under the guidelines and in accordance with the most *
+ * current version of the MIT License.                            *
+ * http://www.opensource.org/licenses/MIT                         *
+ *                                                                *
+ * Example expressions:                                           *
+ * (3 + 4 / 2) * (6 - 2 / 2)                                      *
+ * (3 ^ 2 / 2)                                                    *
+ ******************************************************************
+*/
+
 #pragma once
+
 #include <string>
 #include <iostream>
 
